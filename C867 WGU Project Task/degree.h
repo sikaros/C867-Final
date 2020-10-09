630b70bb9c68753bb5ca611393a360d46d6b651b
sikaros <69648683+sikaros@users.noreply.github.com>
2020-10-08 19:44:36 -0500

    Update degree.h
    final touches

--- C867 WGU Project Task/degree.h
+++ C867 WGU Project Task/degree.h
@@ -2,14 +2,17 @@
 #include <string>
 #include <iostream>
 #include <iomanip>
-
 using namespace std;
+
+/* JYM1 — JYM1 TASK 1: CLASS ROSTER
+SCRIPTING AND PROGRAMMING - APPLICATIONS — C867
+STUDENT : OSCAR AYALA -10/2020
+Student ID: 000485711
+B.S. Data Management/Analytics
+*/
 
 
 enum DegreeProgram { NETWORK, SECURITY, SOFTWARE };
 static const string degreeprogstrings[] = { "NETWORK", "SECURITY", "SOFTWARE" };
-
-
-
 
 
