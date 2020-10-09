78c5734b4c09e8174c2e664e5c911794c4800a17
sikaros <69648683+sikaros@users.noreply.github.com>
2020-10-08 21:29:20 -0500

    Update main.cpp

--- C867 WGU Project Task/main.cpp
+++ C867 WGU Project Task/main.cpp
@@ -9,7 +9,7 @@ int main() { //OBJECTIVE F. create a main function.
 
 	{ "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
 		"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-		"A5,Oscar,Ayala,oayala@wgu.edu,31,20,33,64,SOFTWARE" //OBJECTIVE A.  Modify the “studentData Table” to include your personal information as the last item.
+		"A5,DUMMY,ACCOUNT,LOLIPOP@wgu.edu,31,20,33,64,SOFTWARE" //OBJECTIVE A.  Modify the “studentData Table” to include your personal information as the last item.
 
 
 	};
@@ -21,8 +21,8 @@ int main() { //OBJECTIVE F. create a main function.
 	/*Print out to the screen, 
 	via your application, the course title, the programming language used, your WGU student ID, and your name.*/
 	cout << "---------Scripting and Programming - Applications – C867---------------" << endl;
-	cout << "-----------------------MADE WITH C++ BY OSCAR AYALA -------------------" << endl;
-	cout << "--------------------STUDENT ID:  000485711 ---------------------------" << endl << endl;;
+	cout << "-----------------------MADE WITH C++ BY SIKAROS -------------------" << endl;
+	cout << "--------------------STUDENT ID:  99999999 ---------------------------" << endl << endl;;
 
 	for (int i = 0; i < numStudents; i++)classRoster.parseData(studentData[i]);
 	cout << "CURRENT STUDENT ROSTER:  " << endl;
