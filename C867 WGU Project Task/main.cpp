3238a8721890520cf403a1a5f492c3313cb34dc0
sikaros <69648683+sikaros@users.noreply.github.com>
2020-10-08 21:27:29 -0500

    Update main.cpp

--- C867 WGU Project Task/main.cpp
+++ C867 WGU Project Task/main.cpp
@@ -1,12 +1,16 @@
 #include "roster.h"
 
+// OBBJECTIVE B.  Create a C++ project in your integrated development environment (IDE) with the following files:
 
-int main() {
+
+
+int main() { //OBJECTIVE F. create a main function.
 	const string studentData[] =
 
 	{ "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
 		"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-		"A5,Oscar,Ayala,oayala@wgu.edu,31,20,33,64,SOFTWARE"
+		"A5,Oscar,Ayala,oayala@wgu.edu,31,20,33,64,SOFTWARE" //OBJECTIVE A.  Modify the “studentData Table” to include your personal information as the last item.
+
 
 	};
 
@@ -33,13 +37,13 @@ int main() {
 
 
 	////loop through classRosterArray and for each element:
-	cout << " SHOWING AVERAGE COURSE DAYS FOR ALL STUDENTS:  " << endl;
-	classRoster.printAvgDaysInCourse();
+	cout << " SHOWING AVERAGE COURSE DAYS FOR THIS STUDENT:  " << endl;
+	classRoster.printAvgDaysInCourse("A5");
 	cout << endl;
 	
 
 	
-	classRoster.remove("A3");
+	classRoster.remove("A3"); //this will remove  student with ID 'A3'
 
 	classRoster.printAll();
 
